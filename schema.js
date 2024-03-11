586e6a45f83202270faa8f8b0464aa772cee4b7f
manasip123 <130458536+manasip123@users.noreply.github.com>
2024-03-11 14:28:41 +0530

    Add files via upload

---
+++ schema.js
@@ -0,0 +1,7 @@
+3. Object Relational Mapping (ORM) is a technique used in creating a "bridge" between object-oriented programs and, in most cases, relational databases.
+
+CREATE SCHEMA xyz;
+
+SELECT * FROM sys.schemas
+
+CREATE TABLE schemaname.tablename(values...);
